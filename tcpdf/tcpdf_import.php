fcd0c11e4c4995e9fb8a5774e762c741a39f444d
MD-TANVIR7462 <mdtanvir7462@gmail.com>
2025-07-16 16:44:42 +0600

    i-485 page update

--- tcpdf/tcpdf_import.php
+++ tcpdf/tcpdf_import.php
@@ -1,37 +1,6 @@
 <?php
 //============================================================+
-// File name   : tcpdf_import.php
-// Version     : 1.0.001
-// Begin       : 2011-05-23
-// Last Update : 2013-09-17
-// Author      : Nicola Asuni - Tecnick.com LTD - www.tecnick.com - info@tecnick.com
-// License     : GNU-LGPL v3 (http://www.gnu.org/copyleft/lesser.html)
-// -------------------------------------------------------------------
-// Copyright (C) 2011-2013 Nicola Asuni - Tecnick.com LTD
-//
-// This file is part of TCPDF software library.
-//
-// TCPDF is free software: you can redistribute it and/or modify it
-// under the terms of the GNU Lesser General Public License as
-// published by the Free Software Foundation, either version 3 of the
-// License, or (at your option) any later version.
-//
-// TCPDF is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-// See the GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the License
-// along with TCPDF. If not, see
-// <http://www.tecnick.com/pagefiles/tcpdf/LICENSE.TXT>.
-//
-// See LICENSE.TXT file for more information.
-// -------------------------------------------------------------------
-//
-// Description : This is a PHP class extension of the TCPDF library to
-//               import existing PDF documents.
-//
-//============================================================+
+
 
 /**
  * @file
